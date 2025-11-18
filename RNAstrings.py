b7c43b37538b03607ff2563def75f53171c625b2
enaufty <hzlcnr0@gmail.com>
2025-11-19 02:25:06 +0300

    Update RNAstrings.py

--- RNAstrings.py
+++ RNAstrings.py
@@ -1,24 +1,48 @@
 codon_table = {
-    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",               # Ala
-    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",  # Arg
-    "AAU": "N", "AAC": "N",                                      # Asn
-    "GAU": "D", "GAC": "D",                                      # Asp
-    "UGU": "C", "UGC": "C",                                      # Cys
-    "CAA": "Q", "CAG": "Q",                                      # Gln
-    "GAA": "E", "GAG": "E",                                      # Glu
-    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",              # Gly
-    "CAU": "H", "CAC": "H",                                      # His
-    "AUU": "I", "AUC": "I", "AUA": "I",                          # Ile
-    "AUG": "M",                                                  # Met (Start)
-    "UUU": "F", "UUC": "F",                                      # Phe
-    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",              # Pro
-    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "AGU": "S", "AGC": "S",  # Ser
-    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",              # Thr
-    "UGG": "W",                                                  # Trp
-    "UAU": "Y", "UAC": "Y",                                      # Tyr
-    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V"}              # Val
+    # Alanine
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    # Arginine
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R", "AGA": "R", "AGG": "R",
+    # Asparagine
+    "AAU": "N", "AAC": "N",
+    # Aspartic acid
+    "GAU": "D", "GAC": "D",
+    # Cysteine
+    "UGU": "C", "UGC": "C",
+    # Glutamine
+    "CAA": "Q", "CAG": "Q",
+    # Glutamic acid
+    "GAA": "E", "GAG": "E",
+    # Glycine
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+    # Histidine
+    "CAU": "H", "CAC": "H",
+    # Isoleucine
+    "AUU": "I", "AUC": "I", "AUA": "I",
+    # Leucine
+    "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    # Lysine
+    "AAA": "K", "AAG": "K",
+    # Methionine (Start)
+    "AUG": "M",
+    # Phenylalanine
+    "UUU": "F", "UUC": "F",
+    # Proline
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    # Serine
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S", "AGU": "S", "AGC": "S",
+    # Threonine
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    # Tryptophan
+    "UGG": "W",
+    # Tyrosine
+    "UAU": "Y", "UAC": "Y",
+    # Valine
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V"
+}
 stop = {"UAA": "Stop", "UAG": "Stop", "UGA": "Stop"}
 codon_table.update(stop)
+
 
 sequence = "GUUUAUUGGAUGGAAAUUUCUUUUCGUGAAGCUCAAUAUGCUUCUUUUUCUGCUUGGUGGUAUUAA"
 start_index = sequence.find("AUG")
@@ -27,6 +51,8 @@ protein = ""
 while i < len(sequence):
     codon = sequence[i:i + 3]
     aa = codon_table.get(codon, None)
+    if len(codon) < 3:
+        break
     if aa is None:
         print("Unknown:", codon)
         break
